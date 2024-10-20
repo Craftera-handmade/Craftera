3bbea40a57884af244ec0d2ab68f677990f55c72
Phongldm <leminhphong03@gmail.com>
2024-10-20 17:28:27 +0700

    update page

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => alert("Thank you! your form is submitted successfully." ))
+        .then(response => alert("Cảm ơn bạn đã mua hàng. Chúng mình sẽ liên hệ sớm nhất." + "\n(Bấm OK khi đã hoàn thành chuyển khoản)"))
         .then(() => { window.location.reload(); })
         .catch(error => console.error('Error!', error.message))
 })
